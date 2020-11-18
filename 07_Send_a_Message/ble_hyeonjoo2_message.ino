6980c69f1112e074cb42ea2cd56e074782603474
Hyeonzoo <guswn971205@gmail.com>
2020-11-18 17:57:03 +0900

    오렌지보드에서 메세지 받는 코드 추가

---
+++ 07_Send_a_Message/ble_hyeonjoo2_message.ino
@@ -0,0 +1,35 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial BTSerial(4, 5);   
+String dev = "HJ02";
+
+void setup() {  
+  Serial.begin(9600);
+  BTSerial.begin(9600);
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  if (BTSerial.available()){
+    String data = BTSerial.readString();
+    Serial.print(data);
+    BTSerial.print(data);
+    
+    
+    if(data == '1'){
+      digitalWrite(13, HIGH);
+
+      //String toSend = dev + "_ON";
+      //Serial.print(toSend);
+      //BTSerial.print(toSend);
+    }
+
+    else if(data == '0'){
+      digitalWrite(13, LOW);
+      
+      //String toSend = dev + "_OFF";
+      //Serial.print(toSend);
+      //BTSerial.print(toSend);
+    }
+  }
+}
